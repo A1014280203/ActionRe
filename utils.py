57a056f3a23179e9257a2f302ef89bff6a16c213
A1014280203 <1014280203@qq.com>
2019-08-07 21:37:22 +0800

    Add files via upload

--- utils.py
+++ utils.py
@@ -1,9 +1,10 @@
 import requests
-
+import numpy as np
+import os
 
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
 
-URL = 'http://picture.ik123.com/uploads/allimg/170710/12-1FG0140P6.jpg'
+URL = 'https://p2-1252380913.cos.ap-shanghai.myqcloud.com/sample.jpg'
 
 
 def get_data():
@@ -12,6 +13,44 @@ def get_data():
     except Exception:
         return None
     if resp.status_code == 200:
-        return {'img_b': resp.content}
+        pose = np.load('./1.npy', allow_pickle=True)[0, :2, 0, :].transpose(1, 0)
+        return {'img_b': resp.content,
+                'pose': pose,
+                'boundingBox': [39, 917, 336, 886],
+                'nameAndAction': ['葛某', '走']}
     else:
         return None
+
+
+def load_local_data():
+    imgs = os.listdir('./20_imga/')
+    poses = np.load('./20.npy', allow_pickle=True)
+    i = 0
+
+    def iner():
+        nonlocal imgs, i, poses
+        if i >= len(imgs):
+            return None
+        with open('./20_imga/'+imgs[i], 'rb') as fr:
+            img_b = fr.read()
+        pose = poses[0, :2, 0, :].transpose(1, 0)
+        i += 1
+        return {'img_b': img_b,
+                'pose': pose,
+                'boundingBox': [39, 917, 336, 886],
+                'nameAndAction': ['葛某', '走']}
+    return iner
+
+
+# for test
+get_data = load_local_data()
+
+
+if __name__ == '__main__':
+    # import cv2
+    #
+    # img_b = get_data()['img_b']
+    # img_np = np.frombuffer(img_b, dtype=np.uint8)
+    # img_cv = cv2.imdecode(img_np, cv2.IMREAD_ANYCOLOR)
+    # img_clip = img_cv[39:39+886, 917:917+336]
+    load_local_data()
--- wind.py
+++ wind.py
@@ -1,36 +1,30 @@
 import sys
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import (QWidget, QLCDNumber, QSlider,
-    QVBoxLayout, QApplication, QMainWindow, QHBoxLayout, QLabel, QGraphicsItem)
+from PyQt5.QtCore import Qt, QRect, QSize, QTimer, QBasicTimer
+from PyQt5.QtWidgets import (QWidget, QLCDNumber, QSlider,QFrame,
+    QVBoxLayout, QApplication, QMainWindow, QHBoxLayout, QLabel, QGraphicsItem, QGridLayout)
 from PyQt5.QtGui import QPixmap, QImage
 import utils
+import Visualization
+import cv2
+import matplotlib.pyplot as plt
+import struct
+from PIL import Image
+from io import BytesIO
+from io import BytesIO
 
 
-class ActionRe(QMainWindow):
+class ActionRe(QWidget):
 
     def __init__(self):
         super().__init__()
         self.lb = QLabel(self)
+        self.view_img = QLabel(self)
+        self.view_tag_per = QLabel(self)
+        self.view_tag_ac = QLabel(self)
         self.initUI()
         self.set_data()
-
-    def set_default_image(self):
-        pix = QPixmap(960, 540)
-        pix.fill(Qt.gray)
-        self.lb.setPixmap(pix)
-
-    def set_data(self):
-        data = utils.get_data()
-        img_b = data['img_b']
-        if img_b is None:
-            self.set_default_image()
-        else:
-            img = QImage.fromData(img_b)
-            pix = QPixmap.fromImage(img)
-            self.lb.setPixmap(pix)
-
-    def set_next_data(self):
-        self.set_data()
+        self.timer = QBasicTimer()
+        self.timer.start(1000, self)
 
     def initUI(self):
         self.resize(1120, 630)
@@ -38,16 +32,74 @@ class ActionRe(QMainWindow):
         self.lb.setLineWidth(0)
         self.lb.setGeometry(0, 0, 960, 540)
         self.lb.setStyleSheet("border: 0px")
-
+        self.lb.setScaledContents(True)
         self.set_default_image()
 
+        self.view_img.setGeometry(960+20, 15, 60, 60)
+        self.view_tag_per.setGeometry(960+90, 20, 50, 20)
+        self.view_tag_ac.setGeometry(960+90, 50, 50, 20)
+
         self.show()
+
+    def timerEvent(self, a0):
+        self.set_next_data()
+
+    def set_default_image(self):
+        pix = QPixmap(960, 540)
+        pix.fill(Qt.gray)
+        self.lb.setPixmap(pix)
+        self.view_img.clear()
+
+    def set_data(self):
+        data = utils.get_data()
+
+        if data is None:
+            self.timer.stop()
+            self.set_default_image()
+            self.set_view_person()
+            self.set_view_action()
+        else:
+            # 大图
+            img_b = data['img_b']
+            pose = data['pose']
+            img_np = Visualization.np.frombuffer(img_b, Visualization.np.uint8)
+            img_cv = cv2.imdecode(img_np, cv2.IMREAD_ANYCOLOR)
+            img_cv = Visualization.render(img_cv, pose, Visualization.RENDER_CONFIG_OPENPOSE)
+            cv2.imwrite('s.jpg', img_cv)
+            img_cv = cv2.cvtColor(img_cv, cv2.COLOR_BGR2RGB)
+            pix = Image.fromarray(img_cv).toqpixmap()
+            self.lb.setPixmap(pix)
+            # 小图
+            top, left, width, height = data['boundingBox']
+            img_flip = img_cv[top:top+height, left:left+width]
+            person, action = data['nameAndAction']
+            self.set_view_image(Image.fromarray(img_flip).toqpixmap())
+            self.set_view_person(person)
+            self.set_view_action(action)
+
+    def set_next_data(self):
+        self.set_data()
 
     def keyPressEvent(self, e):
         if e.key() == Qt.Key_Escape:
             self.close()
         if e.key() == Qt.Key_Space:
             self.set_next_data()
+
+    def set_view_image(self, img: QPixmap):
+        w = img.size().width()
+        h = img.size().height()
+        if w > h:
+            pix = img.scaledToWidth(60)
+        else:
+            pix = img.scaledToHeight(60)
+        self.view_img.setPixmap(pix)
+
+    def set_view_action(self, ac: str = ''):
+        self.view_tag_ac.setText(ac)
+
+    def set_view_person(self, per: str = ''):
+        self.view_tag_per.setText(per)
 
 
 if __name__ == '__main__':
